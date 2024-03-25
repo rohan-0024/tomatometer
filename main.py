d2ba2a8e435321263c5131aafbbb7446e28ea93d
rohan-0024 <rohanpaul2468@gmail.com>
2024-03-26 01:05:48 +0530

    Rewriting the code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #print("Hello Everyone...This is one of my project where you can see the rotten tomato score of an movie ")
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import requests
 
-url= 'https://www.rottentomatoes.com'
-source = requests.get(url,'lmxl')
-print(source.text)
+source= requests.get('https://www.rottentomatoes.com/').text
+soup=BeautifulSoup(source,'lmxl')
+print(soup.prettify())
