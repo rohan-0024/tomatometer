fb6138a6791f524bdaca6cbff1939c779a8a1eaa
rohan-0024 <rohanpaul2468@gmail.com>
2024-03-27 01:51:00 +0530

    Entered into container class

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #print("Hello Everyone...This is one of my project where you can see the rotten tomato score of an movie ")
 from bs4 import BeautifulSoup
 import requests
+print("Enter a movie name: ")
+title= input()
+source= requests.get('https://www.rottentomatoes.com/{title}').text
+soup=BeautifulSoup(source, 'html.parser')
 
-source= requests.get('https://www.rottentomatoes.com/').text
-soup=BeautifulSoup(source,'lmxl')
-print(soup.prettify())
+div= soup.find('div',class_='container rt-layout__body').main
+div2=div.find('div')
+print(div2.prettify())
+
+# main= div.find('main',id_='main_container',class_='container rt-layout__body')
+# print(main)
